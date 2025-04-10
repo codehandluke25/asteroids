da61f22dcfdabe5abf560906bd31edf6df6ab153
codehandluke25 <devinpcarroll@gmail.com>
2025-04-10 18:09:54 +0100

    Added limiter for frame rate. of 60, added new file for circle objects

--- Main.py
+++ Main.py
@@ -1,14 +1,21 @@
 import pygame
 from constants import *
+
+
+
 def main():
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     BLACK = 0,0,0
+    dt = 0
+    game_clock = pygame.time.Clock()
+    
     while True:
         screen.fill(BLACK)
         pygame.display.flip()
+        dt =   game_clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
