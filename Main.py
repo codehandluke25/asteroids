dbbe48b12ef74df6c6cd40248b87a7caae267d45
codehandluke25 <devinpcarroll@gmail.com>
2025-04-10 17:46:49 +0100

    Created core game loop, set Screen variables, imported pygame and screen constants, added quit mechanism in for loop.

---
+++ Main.py
@@ -0,0 +1,16 @@
+import pygame
+from constants import *
+def main():
+    print ("Starting Asteroids!")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    BLACK = 0,0,0
+    while True:
+        screen.fill(BLACK)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+if __name__ == "__main__":
+    main()
